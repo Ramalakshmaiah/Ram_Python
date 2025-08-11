f809576fbe0446d74e1f169d60114d287a8db3d8
Ramalakshmaiah <tirupathilakshmaiiah@gmail.com>
2025-08-11 20:20:51 +0530

    Tables_program

---
+++ Bacis_tables.py
@@ -0,0 +1,16 @@
+
+# for i in range(1,6):
+#     for j in range(1,11):
+#         print(f"{i} X {j} = ",i*j)
+
+n = int(input("Enter a number: "))
+
+# i = 1
+
+# while i <=10:
+#     print(f"{n}x{i} = {n*i}")
+
+#     i+=1
+
+for i in range(1,11):
+    print(f"{n}x{i} = {n*i}")
