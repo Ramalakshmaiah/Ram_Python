a86c123bff898a226e2cffd83fe02cb65dead5fc
Ramalakshmaiah <tirupathilakshmaiiah@gmail.com>
2025-09-09 23:01:00 +0530

    while_loop

---
+++ while.py
@@ -0,0 +1,25 @@
+i = 1
+
+while i <= 5:
+    print("ramtirupathi4",i)
+
+    i+=1
+i = 5
+
+while i >= 1:
+    print("Bhagyaram",i)
+
+    i-=1
+
+i = 1
+
+while i <=1:
+    print("bhagya", end =" ")
+
+    j = 1
+    while j<=4:
+        print("ram", end=" ")
+
+        j+=1
+    i+=1
+    
