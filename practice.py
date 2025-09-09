afdaaeccf61148573d93a531bb73e18644167c38
Ramalakshmaiah <tirupathilakshmaiiah@gmail.com>
2025-09-09 23:00:27 +0530

    my_practice

---
+++ practice.py
@@ -0,0 +1,29 @@
+# MESSAGE = "ADAS_MSG"
+
+# count = 0
+
+# if count < 10:
+#     print("MESSAGE")
+#     count= count+1
+# print("Message ended")
+
+# num = [12,24,31,13,22]
+
+# for i in num:
+
+#     if i % 5==0:
+#         print(i)
+#         break
+# else:
+#     print("Not found")
+
+num = 10
+
+for i in range(2,num):
+
+    if num % i==0:
+        print("not prime")
+        break
+else:
+
+    print("prime")
