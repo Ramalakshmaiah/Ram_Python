d8dbf54ca576f51ff6bd2f3431f71122e5d7e4e9
Ramalakshmaiah <tirupathilakshmaiiah@gmail.com>
2025-09-13 21:21:16 +0530

    pytest_group_all_testcases

---
+++ pytest/grouping.py
@@ -0,0 +1,13 @@
+class TestMath:
+
+    def test_add(self):
+        assert 4 + 6 ==10
+
+    def test_sub(self):
+        assert 10 - 4 == 6
+    
+    def test_mult(self):
+        assert 4 * 5 == 20
+
+    def test_div(self):
+        assert 30/6 == 5
