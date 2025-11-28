3f0d6001a04f445eac0f26058608c27166b2fba9
Ramalakshmaiah <tirupathilakshmaiiah@gmail.com>
2025-11-28 13:05:39 +0530

    modi_fabo

--- fabonacci.py
+++ fabonacci.py
@@ -1,9 +1,37 @@
 
-n = int(input("Enteer a number: "))
+# # n = int(input("Enteer a number: "))
 
-a,b = 0,1
+# # a,b = 0,1
 
-while a < n:
-    print(a,end="")
+# # while a < n:
+# #     print(a,end =" ")
     
-    a,b = b,a+b
+# #     a,b = b,a+b
+
+# def feb(n):
+#     a = 0
+#     b = 1
+
+#     if n==1:
+#         print(a)
+
+#     else:
+#         print(a)
+#         print(b)
+
+#         for i in range(2,n):
+#             c = a + b
+#             a = b
+#             b = c
+#             print(c)
+# feb(10)
+
+def fact(n):
+
+    if n == 0:
+        return 1
+    return n * fact(n-1)
+
+result = fact(5)
+
+print(result)
